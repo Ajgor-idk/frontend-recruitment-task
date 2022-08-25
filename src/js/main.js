e901a114b43904abcef3d72efb89857f937c9052
Igor Owczarz <98612273+Ajgor-idk@users.noreply.github.com>
2022-08-25 13:05:10 +0200

    Added some styles to the reset button. Added JS so everything is functional now. Separated some functions to utils so I can use them anywhere and they help to make code cleaner. Edited HTML to fit the reset btn.

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,41 @@
-// const querySelect = (elem) => document.querySelector(elem);
+import { querySelect } from "./utils.js";
+import { createElem } from "./utils.js";
 
-const modalBtn = document.querySelector("#modal-btn")
-const modalBck = document.querySelector(".modal-bck")
-const modal = document.querySelector(".modal")
+const modalBtn = querySelect("#modal-btn");
+const modalBck = querySelect(".modal-bck");
+const modal = querySelect(".modal");
 
 modalBtn.addEventListener("click", () => {
-    modal.style.display = "flex"
-})
+  const counter = querySelect("#times-clicked");
+  sessionStorage.clickcount
+    ? (sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1)
+    : (sessionStorage.clickcount = 1);
+
+  if (sessionStorage.clickcount > 5) {
+    const modalInfo = querySelect(".info");
+    const resetBtn = createElem("button");
+    resetBtn.classList.add("reset-btn");
+    resetBtn.id = "reset-count";
+    resetBtn.innerText = "Reset";
+    resetBtn.addEventListener("click", resetCount);
+    resetBtn.addEventListener("click", closeModal);
+    modalInfo.appendChild(resetBtn);
+  }
+  counter.innerText = sessionStorage.clickcount;
+  modal.style.display = "flex";
+});
+
+const resetCount = () => {
+  sessionStorage.clickcount = 0;
+};
 
 const closeModal = () => {
-    modal.style.display = "none"
-}
+  const resetBtn = querySelect("#reset-count");
+  resetBtn?.remove();
+  modal.style.display = "none";
+};
 
-const xSign = document.querySelector(".x-sign")
+const xSign = querySelect(".x-sign");
 
-xSign.addEventListener("click", closeModal)
-modalBck.addEventListener("click", closeModal)
+xSign.addEventListener("click", closeModal);
+modalBck.addEventListener("click", closeModal);
---
+++ src/js/utils.js
@@ -0,0 +1,2 @@
+export const querySelect = (elem) => document.querySelector(elem);
+export const createElem = (elem) => document.createElement(elem);
